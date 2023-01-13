b59f6076813cc0df2df1177f132d37bc7ca71b08
Felipe Diniz <felipedinizdev@gmail.com>
2023-01-13 00:52:28 -0300

    path mapping

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Groups } from "./src/screens/Groups";
+import { Groups } from "@screens/Groups";
 
 export default function App() {
   return <Groups />;
