cfbf28e788665a8cea461fa79ace8ebcb1c09052
Felipe Diniz <felipedinizdev@gmail.com>
2023-01-19 00:53:10 -0300

    Criando Componente Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,12 @@
 import { Header } from '@components/Header';
-
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
