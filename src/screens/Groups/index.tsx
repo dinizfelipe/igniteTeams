a336208a6592515cd68e91975bf99198ac336405
Felipe Diniz <felipedinizdev@gmail.com>
2023-01-24 00:19:46 -0300

    Criando Componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -22,6 +23,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
