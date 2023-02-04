c7116dad6331e38f64536dc623d294fed9108908
Felipe Diniz <felipedinizdev@gmail.com>
2023-02-04 17:17:33 -0300

    Tipando as rotas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
+import { Container } from './styles';
+
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
-import { Container } from './styles';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
@@ -13,7 +15,10 @@ export function Groups() {
   return (
     <Container>
       <Header />
-      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <Highlight
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
