41810bf0aab686da13270b9975f9fe6baf5375c0
Felipe Diniz <felipedinizdev@gmail.com>
2023-02-04 17:28:50 -0300

    Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,6 +2,7 @@ import { Container } from './styles';
 
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -11,6 +12,11 @@ import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -29,7 +35,10 @@ export function Groups() {
         )}
         showsVerticalScrollIndicator={false}
       />
-      <Button title="Criar nova turma" />
+      <Button
+        title="Criar nova turma"
+        onPress={handleNewGroup}
+      />
     </Container>
   );
 }
