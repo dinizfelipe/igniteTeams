766d625034a7013abe634a289e1ca2f1da9927c0
Felipe Diniz <felipedinizdev@gmail.com>
2023-02-24 02:23:11 -0300

    Tratando exceções

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { Container, Content, Icon } from './styles';
 
 import { groupCreate } from '@storage/group/groupCreate';
-
+import { AppError } from '@utils/AppError';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -19,10 +19,19 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+      }
+
       await groupCreate(group);
       navigation.navigate('players', { group });
     } catch (error) {
-      console.log(error)
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 
